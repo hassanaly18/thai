bcb9c7a2e91c66b3ecbd95b58c8742e1fa91087c
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2025-03-02 17:22:53 +0500

    whatsapp fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
-import WhatsAppButton from "@/components/WhatsappButton";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
